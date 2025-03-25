3c4196a4f88ec8b5189190bccfb0d9083ea4bf4c
Arnaud PY <arnaud.py@student-cs.fr>
2025-03-25 15:12:13 +0100

    Build config

--- gourmitton/next.config.ts
+++ gourmitton/next.config.ts
@@ -16,6 +16,36 @@ const nextConfig: NextConfig = {
     ],
     formats : ['image/avif', 'image/webp'],
   },
+  
+  webpack: (config, { isServer }) => {
+    // Only apply to client bundles
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: 25,
+        minSize: 20000,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          framework: {
+            name: 'framework',
+            test: /[\\/]node_modules[\\/](@vercel\/analytics|react|react-dom|scheduler|next|use-subscription)[\\/]/,
+            priority: 40,
+            enforce: true,
+          },
+          commons: {
+            name: 'commons',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 30,
+            minChunks: 2,
+          },
+        },
+      };
+    }
+    
+    // Return the modified config
+    return config;
+  }
 };
 
 export default nextConfig;
